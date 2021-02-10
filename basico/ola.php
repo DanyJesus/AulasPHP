078e789d38d62245908fa68440322247892484fe
Daniele Galvão <56988997+DanyJesus@users.noreply.github.com>
2021-02-10 12:30:12 -0300

    Alteração assets por basico

---
+++ basico/ola.php
@@ -0,0 +1,15 @@
+<div class="titulo">Olá PHP</div>
+<h2>Exemplo B</h2>
+<?php
+
+echo 'Olá';
+echo 'Mundo';
+
+
+?>
+<h2>Exemplo B</h2>
+<?= "Outra forma de me explessar."?>
+<?= "2021-1993"?>
+<?php
+
+phpinfo();
