1c2a807ba8f8333acde6cbc2826c5cb87f54703c
Daniele Galvão <56988997+DanyJesus@users.noreply.github.com>
2021-02-11 12:24:31 -0300

    variavesi

--- variaveis/basico.php
+++ variaveis/basico.php
@@ -13,5 +13,14 @@ echo $soma;
 $soma = 0;
 echo '<br>';
 
- unset($soma); // destroy variavel, remove as configurações d eum avariavel
+ //unset($soma); // destroy variavel, remove as configurações d eum avariavel
 echo isset($soma);//Verifica se a variavel foi iniciada
+echo '<br>';
+
+//Variaveis pr definidas
+echo $_GET;
+echo '<br>';
+var_dump($_GET);
+echo '<br>';
+var_dump($_SERVER["HTTP_HOST"]);
+echo '<br>';
