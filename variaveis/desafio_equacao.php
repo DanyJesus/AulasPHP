c8b603fba277a60abe89fe2ac8b55ea4863d5177
Daniele Galvão <56988997+DanyJesus@users.noreply.github.com>
2021-02-11 13:00:36 -0300

    desafio variavel

---
+++ variaveis/desafio_equacao.php
@@ -0,0 +1,8 @@
+<div class="titulo">Desafio Equação</div>
+<?php
+$num= (((1-5)*(2-7)/2)**2);
+echo $num;
+$num2= ((6*(2+3))**2)/(3*2);
+echo '<br>',$num2;
+$num3= (($num2 - $num)**3)/10**3;
+echo '<br>',$num3;
