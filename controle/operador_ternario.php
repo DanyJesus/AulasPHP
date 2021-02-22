3631e3e760a6e7e38f296ce1fa488c8cc692b346
Daniele Galvão <56988997+DanyJesus@users.noreply.github.com>
2021-02-22 14:24:47 -0300

    operadores

--- controle/operador_ternario.php
+++ controle/operador_ternario.php
@@ -1,2 +1,21 @@
 <div class="titulo">Operador Ternário</div>
 <?php
+$idade= 70;
+$status;
+if($idade>=18){
+    $status= "Maior de Idade";
+}else{
+    $status = "Menor de Idade";
+}
+echo $status, $idade;
+
+$idade= 15;
+
+$status= $idade>=18 ? "Maior de Idade" : "Menor de Idade";
+//      expressão  caso for Verdadeiro     caso for Falso
+//O operador ternario é seguido de ? e logo após :
+echo '<br>'.$status, $idade;
+$idade= 80;
+$status= ($idade >= 18 ?($idade >= 65 ? 'Aposentado' : 'Maior de Idade' ): 'Menor de Idade');
+     //                    1º a ser executado 
+echo '<br>'.$status, $idade;
---
+++ controle/switch.php
@@ -0,0 +1,52 @@
+<div class="titulo">Switch</div>
+<form action="#" method="post">
+    <label for="categoria">Categoria</label>
+    <select id="categoria" name="categoria">
+
+        <option value="luxo">Luxo</option>
+        <option value="suv">SUV</option>
+        <option value="sedan">Sedan</option>
+        <option value="" selected>Escolha Categoria</option>
+    </select>
+
+    <!-- <label for="preco" >Preço</label>
+<input type="number" name="preco" id="preco">
+
+<label for="carro" >Carro</label>
+<select id="categoria" name="categoria">
+    <option value="mercedes"  >Mercedes</option>
+    <option value="renegade" >Renegade</option>
+    <option value="etios" >Etios</option>
+</select> -->
+
+    <button>Enviar</button>
+</form>
+<?php
+
+if (isset($_POST['categoria'])) {
+    $categoria = ucfirst($_POST['categoria']);
+    $preco = 0.0;
+    $carro = '';
+    // if ($categoria === 'Luxo') {
+    //     $carro = 'Mercedes';
+    //     $preco = '250000';
+    // } else if ($categoria === 'Suv') {
+    //     $carro = 'Renegade';
+    //     $preco = '80000';
+    // } else if ($categoria === 'Sedan') {
+    //     $carro = 'Etios';
+    //     $preco = '55000';
+    // } else {
+    //     echo '<span style="color:red;">Categoria invalida</span>';
+    // }
+    // $precoFormatado = number_format($preco, 2, ',', '.');
+    // echo "<p>Carro: $carro<br>Preco: $precoFormatado<br>Categoria:$categoria</p>";
+    switch($categoria){
+        case 'Luxo':
+            $carro = 'Mercedes';
+            $preco = '250000';
+            break;
+            
+
+    }
+}
